94ede3665ff3bb57eb9d6534c53db7b34ef3fceb
ifxc <304133787@qq.com>
2016-01-25 10:45:59 +0800

    share on github

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+var vue = require('vue-loader')
+var webpack = require('webpack')
+
+module.exports = {
+  //页面入口文件配置
+  entry: './example/main.js',
+  //入口文件输出配置
+  output: {
+    path: './static',
+    publicPath: '/static/',
+    filename: 'build.js'
+  },
+  module: {
+    //加载器配置
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.js$/,
+        // excluding some local linked packages.
+        // for normal use cases only node_modules is needed.
+        exclude: /node_modules|vue\/src|vue-router\/|vue-loader\/|vue-hot-reload-api\//,
+        loader: 'babel'
+      },
+      //图片文件使用 url-loader 来处理，小于8kb的直接转为base64
+      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
+    //   ,
+    //   {
+    //    test: /\.less$/,
+    //    loader: "style!css!autoprefixer!less"
+    //  },
+    ]
+  },
+  babel: {
+    presets: ['es2015'],
+    plugins: ['transform-runtime']
+  },
+  //add
+  // watch: true
+};
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.plugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ];
+} else {
+  module.exports.devtool = '#source-map';
+}
